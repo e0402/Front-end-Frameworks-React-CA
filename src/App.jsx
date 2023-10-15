3060f39e5c771e8fb1c9ff8bd14c7eae7773238d
e0402 <100483961+e0402@users.noreply.github.com>
2023-10-15 16:13:51 +0200

    Added new route paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/HomePage";
 import ProductPage from './pages/ProductPage';
+import CheckoutPage from './pages/CheckoutPage';
+import CheckoutSuccessPage from "./pages/CheckoutSucessPage";
+import ContactForm from './pages/ContactPage';
 import Layout from "./components/common/Layout";
+import { CartProvider } from "./contexts/CartContext";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:productId" element={<ProductPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:productId" element={<ProductPage />} />
+            <Route path="/checkout-success" element={<CheckoutSuccessPage />} />
+            <Route path="/contact" element={<ContactForm />} />
+            <Route path="/cart" element={<CheckoutPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </CartProvider>
   );
 }
 
