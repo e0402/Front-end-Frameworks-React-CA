35baa2a33ac1c9cda7c598517305625704e25b3e
e0402 <100483961+e0402@users.noreply.github.com>
2023-10-15 16:16:08 +0200

    Added footer component

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Header/Navbar";
+import Footer from "./Footer/StyledFooter";
 
 function Layout({ children }) {
   return (
     <div>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 }
